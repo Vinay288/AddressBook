e4bedad4c320a1f35c76c5b1731169b3d53491ea
Vinay288 <vinayhiremath288@gmail.com>
2021-11-16 13:11:06 +0530

    ADDED:axios method to put

---
+++ src/services/addressbook-service.js
@@ -0,0 +1,34 @@
+import config from "../config/config"
+
+const axios=require('axios').default;
+
+export default class AddressBookService{
+
+    baseUrl=config.baseUrl;
+    addAddressBook(data)
+    {
+        console.log("push data")
+        return axios.post(`${this.baseUrl}`,data);
+    }
+    getAllEmployees()
+    {
+        console.log("get data")
+        return axios.get(`${this.baseUrl}`);
+    }
+    deleteEmployee(id)
+    {
+        console.log("deleted")
+        axios.delete(`${this.baseUrl}delete/`+id);
+    }
+    updateEmployee(data,id)
+    {
+        console.log("update")
+        axios.put(`${this.baseUrl}update/`+id,data);
+    }
+    getEmployee(id){
+        console.log("get data for one")
+        return axios.get(`${this.baseUrl}get/`+id);
+    }
+}
+
+
