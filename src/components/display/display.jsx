a2d6fed78cb9db22252f6c74fa665ddf15f27e52
Vinay288 <vinayhiremath288@gmail.com>
2021-11-16 13:48:29 +0530

    ADDED:homepage to dispaly all contacts

---
+++ src/components/display/display.jsx
@@ -0,0 +1,48 @@
+import deleteIcon from '../../assets/icons/delete-black-18dp.svg'
+import React, { useState } from 'react';
+import editIcon from '../../assets/icons/create-black-18dp.svg'
+import '../home/home.scss'
+import { Link, useNavigate } from 'react-router-dom'
+import AddressBookService from '../../services/addressbook-service';
+var addbressbook = new AddressBookService
+
+const Display = (props) => {
+    const remove = (id) => {
+        // employee.deleteEmployee(id)
+        window.location.reload();
+
+    }
+    
+    return (
+        <table id="display" className="table">
+            <tbody>
+                <tr>
+                    <th>Fullname</th>
+                    <th>Address</th>
+                    <th>City</th>
+                    <th>State</th>
+                    <th>Zip Code</th>
+                    <th>Phone Number</th>
+                    <th>Actions</th>
+                </tr>
+                {
+                    props.employeeArray && props.employeeArray.map((element, ind) => (
+                        <tr key={ind}>
+                            <td>{element.name}</td>
+                            <td>{element.address}</td>
+                            <td>{element.city}</td>
+                            <td>{element.state}</td>
+                            <td>{element.zipCode}</td>
+                            <td>{element.phoneNumber}</td>
+                            <td><img onClick={() => remove(element.id)} src={deleteIcon} alt="delete" />
+                                <Link to={"/form/" + (element.id)}> <img src={editIcon} alt="edit" /></Link>
+                            </td>
+                        </tr>
+
+                    ))
+                }
+            </tbody>
+        </table>
+    );
+}
+export default Display;
