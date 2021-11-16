556c4fc3bd96546bfccd946669bfb399cb9fbebe
Vinay288 <vinayhiremath288@gmail.com>
2021-11-16 14:53:43 +0530

    ADDED: update method

--- src/components/addressbook-form/addressbook-form.jsx
+++ src/components/addressbook-form/addressbook-form.jsx
@@ -1,11 +1,14 @@
 import "../addressbook-form/addressbook-form.scss";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate , useParams } from 'react-router-dom'
+import logo from "../../assets/contact_logo.png"
 import { useState } from "react";
 import AddressBookService from "../../services/addressbook-service";
 import { v1 as uuidv1 } from 'uuid';
 var addressbook = new AddressBookService();
+
 const AddressBookForm = (props) => {
     const navigate = useNavigate();
+    const { id } = useParams()
     let initialValue = {
         name: '',
         phoneNumber: '',
@@ -75,6 +78,25 @@ const AddressBookForm = (props) => {
         await setForm({ ...formValue, error: error })
         return isError;
     }
+    const update = () => {
+        if (id) {
+            formValue.isUpdate = true
+            addressbook.getContact(id).then(contact => {
+                setForm({
+                    ...formValue,
+                    name: contact.data.name,
+                    phoneNumber: contact.data.phoneNumber,
+                    address: contact.data.address,
+                    city: contact.data.city,
+                    state: contact.data.state,
+                    zipCode: contact.data.zipCode,
+
+                });
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    }
 
     const save = async (event) => {
         event.preventDefault();
@@ -84,34 +106,53 @@ const AddressBookForm = (props) => {
             console.log('error', formValue);
             return;
         }
+        if(formValue.isUpdate){
         let object = {
             name: formValue.name,
             phoneNumber: formValue.phoneNumber,
             address: formValue.address,
             city: formValue.city,
             state: formValue.state,
-            zipCode: formValue.zipCode,
-            id: uuidv1(),
+            zipCode: formValue.zipCode
         }
-        console.log(object)
-        addressbook.addContact(object).then(data => {
-            console.log("data added successfully");
+        console.log("in update")
+        await addressbook.updateContact(object,id).then(data => {
+            console.log("data added");
         }).catch(err => {
-            console.log("error while adding data ", err);
+            console.log("error during update", err);
         })
+    }
+        
+        else {
+            let object = {
+                name: formValue.name,
+                phoneNumber: formValue.phoneNumber,
+                address: formValue.address,
+                city: formValue.city,
+                state: formValue.state,
+                zipCode: formValue.zipCode,
+                id:uuidv1()
+            }
+            console.log(object)
+            await addressbook.addContact(object).then(data => {
+                console.log("data added");
+            }).catch(err => {
+                console.log("error while add", err);
+            })
+        }
+        
         navigate('/home')
     }
 
     const reset = () => {
         setForm({ ...initialValue, id: formValue.id, isUpdate: formValue.isUpdate })
-        console.log(formValue)
     }
 
     return (
         <div>
-            <header className="header-content header">
+            <header className="header-content header" onLoad={update}>
                 <div className="logo-content">
-                    <img src="assets/contact_logo.png" alt="address-book-logo" />
+                    <img src={logo} alt="address-book-logo" />
                     <div>
                         <span className="address-text">ADDRESS</span><br />
                         <span className="address-text address-book">BOOK</span>
@@ -127,18 +168,18 @@ const AddressBookForm = (props) => {
                     </div>
                     <div className="row-content">
                         <label className="label text" htmlFor="name">Full Name</label>
-                        <input className="input" type="text" id="name" name="name" onChange={changeValue} autocomplete="disable" required />
+                        <input className="input" type="text" id="name" name="name" value={formValue.name} onChange={changeValue} autocomplete="disable" required />
                         <div className="error" id="name-error">{formValue.error.name}</div>
                     </div>
                     <div className="row-content">
                         <label className="label text" htmlFor="phoneNumber">Phone Number</label>
-                        <input className="input" type="tel" id="phoneNumber" name="phoneNumber" onChange={changeValue} autocomplete="disable" required />
+                        <input className="input" type="tel" id="phoneNumber" name="phoneNumber" value={formValue.phoneNumber} onChange={changeValue} autocomplete="disable" required />
                         <div className="error" id="phoneNumber-error">{formValue.error.phoneNumber}</div>
                     </div>
                     <div className="row-content">
                         <div className="text-row">
                             <label className="label text" htmlFor="address">Address</label>
-                            <textarea id="address" className="input" name="address" onChange={changeValue} placeholder="" style={{ height: "100px" }} autocomplete="disable"></textarea>
+                            <textarea id="address" className="input" name="address" value={formValue.address} onChange={changeValue} placeholder="" style={{ height: "100px" }} autocomplete="disable"></textarea>
                             <div className="error" id="address-error">{formValue.error.address}</div>
 
 
@@ -147,7 +188,7 @@ const AddressBookForm = (props) => {
                     <div className="row-content location-row">
                         <div>
                             <label className="label text" htmlFor="city">City</label>
-                            <select id="city" onChange={changeValue} name="city">
+                            <select id="city" value={formValue.city} onChange={changeValue} name="city">
                                 <option value="" disabled selected hidden>Select City</option>
                                 <option value="Bangalore">Bangalore</option>
                                 <option value="Mumbai">Mumbai</option>
@@ -158,7 +199,7 @@ const AddressBookForm = (props) => {
                         </div>
                         <div className="state-row">
                             <label className="label text" htmlFor="state">State</label>
-                            <select id="state" onChange={changeValue} name="state">
+                            <select id="state" value={formValue.state} onChange={changeValue} name="state">
                                 <option value="" disabled selected hidden>Select State</option>
                                 <option value="Karnataka">Karnataka</option>
                                 <option value="Maharastra">Maharastra</option>
@@ -168,7 +209,7 @@ const AddressBookForm = (props) => {
                         </div>
                         <div>
                             <label className="label text" htmlFor="zip">Zipcode</label>
-                            <input className="input" type="text" id="zip" name="zipCode" onChange={changeValue} required autocomplete="disable" />
+                            <input className="input" type="text" id="zip" name="zipCode" value={formValue.zipCode} onChange={changeValue} required autocomplete="disable" />
                             <div className="error" id="zip-error">{formValue.error.zipCode}</div>
 
 
@@ -179,7 +220,7 @@ const AddressBookForm = (props) => {
                             <button type="submit" class="button submitButton" id="submitButton" onClick={save}>
                                 Add
                             </button>
-                            <button type="reset" class="resetButton button" id="resetButton" onclick="reset()"
+                            <button type="reset" class="resetButton button" id="resetButton" onClick={reset}
                             >Reset</button>
                         </div>
                     </div>
